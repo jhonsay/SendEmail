5b1088a409799368fa9febf7c16fd63425bf4d60
Pavel <nichitapavel@live.com>
2015-05-31 00:42:10 +0100

    Añadido prueba unitaria JUnit

---
+++ src/test/java/enviartest/EnviarTest.java
@@ -0,0 +1,14 @@
+package enviartest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class EnviarTest
+{
+  
+    @Test
+    public void firstTest()
+    {
+        assertEquals("a", "a");
+    }
+}
