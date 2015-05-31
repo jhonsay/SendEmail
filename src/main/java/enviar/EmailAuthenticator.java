17eb32e1b0c6ebd057929cb054ae1d7afdffe93e
Pavel <nichitapavel@live.com>
2015-05-31 01:52:54 +0100

    metodo hashCode y equals para EmailAuthenticator

--- src/main/java/enviar/EmailAuthenticator.java
+++ src/main/java/enviar/EmailAuthenticator.java
@@ -17,4 +17,35 @@ public class EmailAuthenticator extends Authenticator {
         return new PasswordAuthentication(username, password);
      }
 
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((password == null) ? 0 : password.hashCode());
+        result = prime * result
+                + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EmailAuthenticator other = (EmailAuthenticator) obj;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+
 }
