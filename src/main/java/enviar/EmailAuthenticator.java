21e754aa3622dd76b4ebc6d6d1876fa27660a348
Pavel <nichitapavel@live.com>
2015-05-31 03:09:46 +0100

    añadido javadoc para EmailAuthenticator

--- src/main/java/enviar/EmailAuthenticator.java
+++ src/main/java/enviar/EmailAuthenticator.java
@@ -3,20 +3,51 @@ package enviar;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 
+/**
+ * 
+ * @author Pavel
+ * @author Cristo
+ * 
+ * Autentica el usuario contra el servidor de correo electronico
+ *
+ */
+
 public class EmailAuthenticator extends Authenticator {
 
+    /**
+     * Nombre de usuario
+     */
     private String username;
+    
+    /**
+     * Contraseña del usuario
+     */
     private String password;
 
+    /**
+     * Constructor publico, asigna los parametros a los atributos de clase
+     * @param username Nombre de usuario
+     * @param password Contraseña del usuario
+     */
     public EmailAuthenticator(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * Crea un objeto con las credenciales del usuario para la autenticación
+     * 
+     * @return PasswordAuthentication
+     */
     public PasswordAuthentication getPasswordAuthentication() {
         return new PasswordAuthentication(username, password);
      }
 
+    /**
+     * Mejora de codigo, calculo el hash del objeto
+     * 
+     * @return int valor del hash calculado
+     */
     public int hashCode() {
         final int prime = 31;
         int result = 1;
@@ -27,6 +58,12 @@ public class EmailAuthenticator extends Authenticator {
         return result;
     }
 
+    /**
+     * Operador de igualdad para los objetos
+     * 
+     * @param obj       objecto de la misma clase EmailAuthenticator
+     * @return boolean  true si son iguales, false si son distintos
+     */
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
